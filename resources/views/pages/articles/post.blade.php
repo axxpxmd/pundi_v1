2c299f9190e0684897d5f9ca946906f74fa0fda0
axxpxmd <asiphamdi@gmail.com>
2021-02-22 23:38:51 +0700

    update

--- resources/views/pages/articles/post.blade.php
+++ resources/views/pages/articles/post.blade.php
@@ -86,53 +86,11 @@
             ['fontsize', ['fontsize']],
             ['fontname', ['fontname']],
             ['color', ['color']],
+            ['insert', ['picture']],
             ['paragraph', ['ul', 'ol', 'paragraph', 'height']],
             ['view', ['fullscreen', 'codeview']]
         ]
     });
-
-    // file name preview
-    (function () {
-        'use strict';
-        $('.input-file').each(function () {
-            var $input = $(this),
-                $label = $input.next('.js-labelFile'),
-                labelVal = $label.html();
-
-            $input.on('change', function (element) {
-                var fileName = '';
-                if (element.target.value) fileName = element.target.value.split('\\').pop();
-                fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label
-                    .removeClass('has-file').html(labelVal);
-            });
-        });
-    })();
-
-    // image preview
-    function tampilkanPreview(gambar, idpreview) {
-        var gb = gambar.files;
-        for (var i = 0; i < gb.length; i++) {
-            var gbPreview = gb[i];
-            var imageType = /image.*/;
-            var preview = document.getElementById(idpreview);
-            var reader = new FileReader();
-            if (gbPreview.type.match(imageType)) {
-                preview.file = gbPreview;
-                reader.onload = (function (element) {
-                    return function (e) {
-                        element.src = e.target.result;
-                    };
-                })(preview);
-                reader.readAsDataURL(gbPreview);
-            } else {
-                Swal.fire(
-                    'Tipe file tidak boleh',
-                    'Harus format gambar',
-                    'error'
-                )
-            }
-        }
-    }
 
     // Sub Category
     $(document).ready(function(){
